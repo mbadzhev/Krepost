7988ee38928117ad521f3eb417bb9ccf705fd344
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-02-17 10:47:16 +0000

    Add text boxes for master password input

--- KrepostWinForms/Forms/NewDatabaseForm.Designer.cs
+++ KrepostWinForms/Forms/NewDatabaseForm.Designer.cs
@@ -28,19 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxPasswordRepeat = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelPasswordRepeat = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(156, 109);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(266, 23);
+            this.textBoxPassword.TabIndex = 0;
+            // 
+            // textBoxPasswordRepeat
+            // 
+            this.textBoxPasswordRepeat.Location = new System.Drawing.Point(156, 149);
+            this.textBoxPasswordRepeat.Name = "textBoxPasswordRepeat";
+            this.textBoxPasswordRepeat.PasswordChar = '*';
+            this.textBoxPasswordRepeat.Size = new System.Drawing.Size(266, 23);
+            this.textBoxPasswordRepeat.TabIndex = 1;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(12, 112);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(129, 15);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Enter Master Password:";
+            // 
+            // labelPasswordRepeat
+            // 
+            this.labelPasswordRepeat.AutoSize = true;
+            this.labelPasswordRepeat.Location = new System.Drawing.Point(12, 152);
+            this.labelPasswordRepeat.Name = "labelPasswordRepeat";
+            this.labelPasswordRepeat.Size = new System.Drawing.Size(138, 15);
+            this.labelPasswordRepeat.TabIndex = 3;
+            this.labelPasswordRepeat.Text = "Repeat Master Password:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "TODO: Explain master password.";
             // 
             // NewDatabaseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(434, 411);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelPasswordRepeat);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxPasswordRepeat);
+            this.Controls.Add(this.textBoxPassword);
             this.Name = "NewDatabaseForm";
             this.Text = "NewDatabaseForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox textBoxPassword;
+        private TextBox textBoxPasswordRepeat;
+        private Label labelPassword;
+        private Label labelPasswordRepeat;
+        private Label label1;
     }
 }
