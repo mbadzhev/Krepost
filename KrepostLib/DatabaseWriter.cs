199da148bb50fcda8053df51ba87970c8815466e
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-02-17 15:58:04 +0000

    Add write db function

--- KrepostLib/DatabaseWriter.cs
+++ KrepostLib/DatabaseWriter.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace KrepostLib
 {
@@ -20,6 +22,21 @@ namespace KrepostLib
             dbHead.integrityHash = "jhfguuhtrkjk";
             db.Items = dbHead;
             return db;
+        }
+
+        public static void WriteDatabase(object obj, string path)
+        {
+            // TODO: Chech for errors in the process
+
+            // Use new line and indentadion in xml output.
+            XmlWriterSettings xmlWriterSettings = new()
+            {
+                Indent = true
+            };
+            XmlSerializer serializer = new(typeof(Database));
+            using XmlWriter xmlWriter = XmlWriter.Create(path, xmlWriterSettings);
+            serializer.Serialize(xmlWriter, obj);
+            xmlWriter.Close();
         }
     }
 }
