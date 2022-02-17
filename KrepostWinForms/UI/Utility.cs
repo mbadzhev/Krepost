73a6ee24f4c3fcf97797e972d3073c0b6ea3eca0
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-02-17 17:10:19 +0000

    Add save db function

---
+++ KrepostWinForms/UI/Utility.cs
@@ -0,0 +1,20 @@
+﻿namespace KrepostWinForms.UI
+{
+    public static class Utility
+    {
+        public static void SaveDatabase()
+        {
+            // Set dialog settings
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            sfd.FilterIndex = 2;
+            sfd.RestoreDirectory = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                // Create db, use the picked path and serialize the db
+                KrepostLib.DatabaseWriter.WriteDatabase(KrepostLib.DatabaseWriter.CreateDatabase(), Path.GetFullPath(sfd.FileName));
+            }
+        }
+    }
+}
