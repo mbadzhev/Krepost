6510f4575f00bbbdaebb25b32a56fb50577dc7ac
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-02-17 15:50:57 +0000

    Add create db function

---
+++ KrepostLib/DatabaseWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrepostLib
+{
+    public static class DatabaseWriter
+    {
+        public static object CreateDatabase()
+        {
+            // TODO: Complete implementation
+            Database db = new Database();
+            DatabaseHead dbHead = new DatabaseHead();
+            dbHead.hashId = "SHA256";
+            dbHead.accessHash = "passwordhash";
+            dbHead.cipherId = "AES256";
+            dbHead.databaseIv = "myIv";
+            dbHead.integrityHash = "jhfguuhtrkjk";
+            db.Items = dbHead;
+            return db;
+        }
+    }
+}
