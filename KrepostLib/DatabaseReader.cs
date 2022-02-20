9378947c4683ce5b217722ec385676fde06dcf65
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-02-20 17:28:01 +0000

    Add function to read whole db file into db object

---
+++ KrepostLib/DatabaseReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+namespace KrepostLib
+{
+    public static class DatabaseReader
+    {
+        public static Database ReadDatabase(string path)
+        {
+            // Create a new file stream for reading the XML file
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+            Database obj;
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.Async = true;
+
+            using (XmlReader xmlReader = XmlReader.Create(fs, settings))
+            {
+                XmlSerializer deserializer = new XmlSerializer(typeof(Database));
+                obj = deserializer.Deserialize(xmlReader) as Database;
+            }
+
+            // Cleanup
+            fs.Close();
+
+            return obj;
+        }
+    }
+}
