9d6dd361648b5fa0a9cf7f4e505c747af87cf2dd
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-02-22 16:07:00 +0000

    Add ui components to form

--- KrepostWinForms/Forms/OpenDatabaseForm.Designer.cs
+++ KrepostWinForms/Forms/OpenDatabaseForm.Designer.cs
@@ -28,20 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.AutoSize = true;
+            this.labelHeader.Location = new System.Drawing.Point(12, 9);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(153, 15);
+            this.labelHeader.TabIndex = 0;
+            this.labelHeader.Text = "Enter Your Master Password";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(117, 83);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(305, 23);
+            this.textBoxPassword.TabIndex = 1;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(12, 86);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(99, 15);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Master Password:";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(347, 326);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 3;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(266, 326);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 4;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
             // 
             // OpenDatabaseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(434, 361);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelHeader);
             this.Name = "OpenDatabaseForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "OpenDatabaseForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label labelHeader;
+        private TextBox textBoxPassword;
+        private Label labelPassword;
+        private Button buttonCancel;
+        private Button buttonOpen;
     }
 }
