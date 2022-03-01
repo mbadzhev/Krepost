f34ec62c383406ff1a675313fa05d8a63b922e07
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-03-01 18:20:13 +0000

    Add function to generate random byte array

---
+++ KrepostLib/Cryptography/Generator.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace KrepostLib.Cryptography
+{
+    public static class Generator
+    {
+        public static byte[] GenerateBytes(int size)
+        {
+            if (size <= 0)
+            {
+                // Can't have less than one byte
+                Debug.Assert(false); 
+                throw new ArgumentOutOfRangeException("size");
+            }
+            int arraySize = size;
+            byte[] byteArray = new byte[arraySize];
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(byteArray);
+            }
+            return byteArray;
+        }
+    }
+}
