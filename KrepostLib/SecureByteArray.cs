7ee82ba0779b20de4488957bd8aab179397d1ce2
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-03-03 17:58:17 +0000

    Add placeholder functions to secure byte array

--- KrepostLib/SecureByteArray.cs
+++ KrepostLib/SecureByteArray.cs
@@ -2,5 +2,25 @@
 {
     public sealed class SecureByteArray
     {
+        public SecureByteArray()
+        {
+            // create byte[]
+            throw new NotImplementedException();
+        }
+        public void Encrypt()
+        {
+            // encrypt byte[]
+            throw new NotImplementedException();
+        }
+        public void Decrypt()
+        {
+            // decrypt byte[]
+            throw new NotImplementedException();
+        }
+        public byte[] Read()
+        {
+            // decrypt byte[], return unsecure byte[] and encrypt byte[] again
+            throw new NotImplementedException();
+        }
     }
 }
