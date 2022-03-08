fa01207115094f66f279e87ebffb6f05552f691b
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-03-08 16:43:20 +0000

    Modify generate bytes function documentation for readability

--- KrepostLib/Cryptography/Generator.cs
+++ KrepostLib/Cryptography/Generator.cs
@@ -5,15 +5,21 @@ namespace KrepostLib.Cryptography
 {
     public static class Generator
     {
-        public static byte[] GenerateBytes(int size)
+        /// <summary>
+        /// Generates cryptographically strong set of bytes.
+        /// </summary>
+        /// <param name="byteNumber">Number of bytes to be generated.</param>
+        /// <returns>A new byte array.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static byte[] GenerateBytes(int byteNumber)
         {
-            if (size <= 0)
+            if (byteNumber <= 0)
             {
                 // Can't have less than one byte
                 Debug.Assert(false); 
                 throw new ArgumentOutOfRangeException("size");
             }
-            int arraySize = size;
+            int arraySize = byteNumber;
             byte[] byteArray = new byte[arraySize];
             using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
             {
