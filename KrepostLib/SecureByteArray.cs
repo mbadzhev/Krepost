7a1f30fbe0a74ab6e41e166d97e94aa9e2d324ff
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-03-09 11:24:32 +0000

    Implement secure byte array encrypt function

--- KrepostLib/SecureByteArray.cs
+++ KrepostLib/SecureByteArray.cs
@@ -1,8 +1,21 @@
-﻿namespace KrepostLib
+﻿using KrepostLib.Cryptography;
+
+namespace KrepostLib
 {
     public sealed class SecureByteArray
     {
         private byte[] data;
+        /// <summary>
+        /// The key member length must be 32 bytes (256 bits).
+        /// </summary>
+        private byte[] dataKey;
+        /// <summary>
+        /// The nonce member length must be 12 bytes (96 bits).
+        /// </summary>
+        private byte[] dataIv;
+        /// <summary>
+        /// The tag member length must be 16 bytes (128 bits).
+        /// </summary>
         private uint dataLenght;
 
         /// <summary>
@@ -44,8 +57,29 @@
         }
         public void Encrypt()
         {
-            // encrypt byte[]
-            throw new NotImplementedException();
+            // Validate at least one element is present for encryption.
+            if (data.Length == 0) return;
+
+            // Validate key has been generated.
+            if (dataKey == null)
+            {
+                // Generate 32 byte (256 bit) byte array.
+                dataKey = Argon2Engine.DeriveKey(Generator.GenerateBytes(32), Generator.GenerateBytes(16));
+            }
+
+            // Validate nonce has been generated.
+            if (dataIv == null)
+            {
+                // Generate 12 byte (96 bit) byte array.
+                dataIv = Generator.GenerateBytes(16);
+            }
+
+            byte[] temp = AesEngine.Encrypt(data, dataKey, dataIv);
+            
+            // Overwrite plaintext data with ciphertext
+            data = null;
+            data = new byte[temp.Length];
+            data = temp;
         }
         public void Decrypt()
         {
