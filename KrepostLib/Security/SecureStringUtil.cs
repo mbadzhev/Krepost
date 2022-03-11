0b7874eea968b6685754b64c7f065cb60a66c6c9
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-03-11 10:10:31 +0000

    Implement secure string util convert to byte array function

--- KrepostLib/Security/SecureStringUtil.cs
+++ KrepostLib/Security/SecureStringUtil.cs
@@ -1,4 +1,7 @@
-﻿using System.Security;
+﻿// Code taken almost in full from https://codereview.stackexchange.com/questions/107860/converting-a-securestring-to-a-byte-array
+
+using System.Runtime.InteropServices;
+using System.Security;
 using System.Text;
 
 namespace KrepostLib.Security
@@ -32,9 +35,42 @@ namespace KrepostLib.Security
             disposedStatus = false;
         }
 
-        public byte[] ToByteArray()
+        public unsafe byte[] SecureStringToByteArray()
         {
-            throw new NotImplementedException();
+            int maxLength = encoding.GetMaxByteCount(data.Length);
+
+            IntPtr bytes = IntPtr.Zero;
+            IntPtr str = IntPtr.Zero;
+
+            try
+            {
+                bytes = Marshal.AllocHGlobal(maxLength);
+                str = Marshal.SecureStringToBSTR(data);
+
+                char* chars = (char*)str.ToPointer();
+                byte* bptr = (byte*)bytes.ToPointer();
+                int len = encoding.GetBytes(chars, data.Length, bptr, maxLength);
+
+                dataByteArray = new byte[len];
+                for (int i = 0; i < len; ++i)
+                {
+                    dataByteArray[i] = *bptr;
+                    bptr++;
+                }
+
+                return dataByteArray;
+            }
+            finally
+            {
+                if (bytes != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(bytes);
+                }
+                if (str != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeBSTR(str);
+                }
+            }
         }
 
         public void Dispose()
