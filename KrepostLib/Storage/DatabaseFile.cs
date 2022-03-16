973796607f03d09a6d28a200e112340f0ebb8118
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-03-16 21:54:14 +0000

    Add database file class to serialize db to file

---
+++ KrepostLib/Storage/DatabaseFile.cs
@@ -0,0 +1,9 @@
+﻿namespace KrepostLib.Storage
+{
+    [Serializable]
+    public sealed class DatabaseFile
+    {
+        public byte[]? Head { get; set; }
+        public byte[]? Body { get; set; }
+    }
+}
