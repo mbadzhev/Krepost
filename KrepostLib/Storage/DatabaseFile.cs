7a41384454963b557d3e8ed95e9b1d06ca02fefe
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-03-17 16:36:26 +0000

    Implement db file integrity through hash of db head and body

--- KrepostLib/Storage/DatabaseFile.cs
+++ KrepostLib/Storage/DatabaseFile.cs
@@ -3,7 +3,29 @@
     [Serializable]
     public sealed class DatabaseFile
     {
+        /// <summary>
+        /// Gets or sets the database head as a byte array.
+        /// </summary>
         public byte[]? Head { get; set; }
+        /// <summary>
+        /// Gets or sets the hash used for validating the integrity of the head.
+        /// </summary>
+        public string? HeadHash { get; set; }
+        /// <summary>
+        /// Gets or sets the encrypted database body as a byte array.
+        /// </summary>
         public byte[]? Body { get; set; }
+        /// <summary>
+        /// Gets or sets the hash used for validating the integrity of the body.
+        /// </summary>
+        public string? BodyHash { get; set; }
+        /// <summary>
+        /// Gets or sets the hash function used to compute the hashes of the head and body.
+        /// </summary>
+        public HashId HashFunction { get; set; }
+        /// <summary>
+        /// Gets or sets the salt used to compute the hashes of the head and body.
+        /// </summary>
+        public byte[]? Salt { get; set; }
     }
 }
