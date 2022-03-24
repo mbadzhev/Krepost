bcaf1a5b2c9962a20101b786ed87ef3dcffa4d3b
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-03-24 18:08:59 +0000

    Add copy of AddEntryForm as a edit entry form

---
+++ KrepostWinForms/Forms/EditEntryForm.cs
@@ -0,0 +1,69 @@
+﻿using KrepostLib.Storage;
+
+namespace KrepostWinForms.Forms
+{
+    public partial class EditEntryForm : Form
+    {
+        public EditEntryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (Program.CurrentDb == null)
+            {
+                MessageBox.Show("No open database is available.", "Krepost", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Check the required entry fields (title, email/username & password)
+            // are filled and not empty.
+            if (!ValidateInput())
+            {
+                return;
+            }
+
+            // Create new database entry with data from the input boxes.
+            DatabaseEntry entry = new DatabaseEntry(textBoxTitle.Text,
+                secureStringTextBoxUsername.ToSecureByteArray(),
+                secureStringTextBoxEmail.ToSecureByteArray(),
+                secureStringTextBoxPassword.ToSecureByteArray(),
+                textBoxUrl.Text,
+                secureStringTextBoxNote.ToSecureByteArray(),
+                KrepostLib.Cryptography.Generator.GenerateBytes(16));
+
+            Program.CurrentDb.Body.EntryList.Add(entry);
+        }
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            secureStringTextBoxUsername.Data.Dispose();
+            secureStringTextBoxEmail.Data.Dispose();
+            secureStringTextBoxPassword.Data.Dispose();
+            secureStringTextBoxNote.Data.Dispose();
+            Close();
+        }
+        private bool ValidateInput()
+        {
+            if (textBoxTitle.Text == null || textBoxTitle.Text.Length <= 0)
+            {
+                MessageBox.Show("No title set for entry. An entry must contain a title.", "Krepost", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if ((secureStringTextBoxUsername.Data == null ||
+                secureStringTextBoxUsername.Data.Length <= 0) &&
+                (secureStringTextBoxEmail.Data == null ||
+                secureStringTextBoxEmail.Data.Length <= 0))
+            {
+                MessageBox.Show("No username or email set for entry. An entry must contain at least one.", "Krepost", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            if (secureStringTextBoxPassword.Data == null || secureStringTextBoxPassword.Data.Length <= 0)
+            {
+                MessageBox.Show("No username set for entry. An entry must contain a title.", "Krepost", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+    }
+}
