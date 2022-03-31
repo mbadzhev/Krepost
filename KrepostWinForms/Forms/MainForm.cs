93cfb12e535789de792b471408a866cdfb8fdc3a
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-03-31 11:47:47 +0100

    Modify the appearance of toolStrip to match the rest of the ui

--- KrepostWinForms/Forms/MainForm.cs
+++ KrepostWinForms/Forms/MainForm.cs
@@ -1,4 +1,5 @@
-﻿namespace KrepostWinForms.Forms
+﻿using KrepostWinForms.UI;
+namespace KrepostWinForms.Forms
 {
     public partial class MainForm : Form
     {
@@ -6,6 +7,7 @@
         {
             InitializeComponent();
 
+            toolStrip.Renderer = new CustomToolStripRenderer();
             //menuStripEntryAddEntry.Enabled = false;
         }
 
---
+++ KrepostWinForms/UI/CustomToolStripRender.cs
@@ -0,0 +1,16 @@
+﻿namespace KrepostWinForms.UI
+{
+    // Taken from https://stackoverflow.com/questions/8527585/how-to-remove-this-strange-visual-artifact-in-the-corner-of-toolstrip-winforms-c
+    internal class CustomToolStripRenderer : ToolStripProfessionalRenderer
+    {
+        public CustomToolStripRenderer() : base(new CustomColorTable())
+        {
+
+        }
+
+        protected override void OnRenderToolStripBorder(ToolStripRenderEventArgs e)
+        {
+            // Prevent default distorted border from being rendered.
+        }
+    }
+}
