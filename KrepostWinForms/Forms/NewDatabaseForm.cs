827d386f116f9f3b9bf787b99af6041eca95153b
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-04-03 14:04:30 +0100

    Implement new func to create db into winform ui

--- KrepostWinForms/Forms/NewDatabaseForm.cs
+++ KrepostWinForms/Forms/NewDatabaseForm.cs
@@ -1,5 +1,7 @@
 ﻿using KrepostLib.Cryptography;
 
+using KrepostWinForms.Middleware;
+
 namespace KrepostWinForms.Forms
 {
     public partial class NewDatabaseForm : Form
@@ -9,32 +11,36 @@ namespace KrepostWinForms.Forms
         {
             InitializeComponent();
 
-            // Generate and set salt to be used for hashing user input
-            salt = KrepostLib.Cryptography.Generator.GenerateBytes(16);
+            // Generate salt for hashing user input.
+            salt = Generator.GenerateBytes(16);
             secureStringTextBoxTop.DataSalt = salt;
             secureStringTextBoxBottom.DataSalt = salt;
         }
         private void buttonSave_Click(object sender, EventArgs e)
         {
-            // TODO: Securely compare the master passwords.
+            // Check that the user input matches accross the text boxes.
             if (!KrepostLib.Utility.CompareStrings(secureStringTextBoxTop.DataHash, secureStringTextBoxBottom.DataHash))
-            {
                 MessageBox.Show("Passwords do not match or are empty.", "Krepost", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
             else
             {
-                byte[] key = Argon2Engine.DeriveKey(secureStringTextBoxTop.ToSecureByteArray(), secureStringTextBoxTop.DataSalt);
-                Program.CurrentKey = new KrepostLib.Security.SecureByteArray(ref key);
-                Array.Clear(key, 0, key.Length);
-                if (UI.Utility.SaveDatabase(secureStringTextBoxTop.DataHash, secureStringTextBoxTop.DataSalt))
+                // Generate encryption key from user input.
+                DatabaseUtils.SetNewKey(secureStringTextBoxTop);
+
+                // Check if key generation failed.
+                if (Program.CurrentKey != null)
                 {
+                    // Create new database and dispose of user input.
+                    DatabaseUtils.NewDatabase(secureStringTextBoxTop.DataHash, Program.CurrentKey, secureStringTextBoxTop.DataSalt);
                     secureStringTextBoxTop.Data.Dispose();
                     secureStringTextBoxBottom.Data.Dispose();
                     Close();
+                    return;
                 }
+                MessageBox.Show("Entryption key has not created correctly.A new database has not been created.",
+                    "Krepost", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
             }
         }
-
         private void buttonCancel_Click(object sender, EventArgs e)
         {
             secureStringTextBoxTop.Data.Dispose();
