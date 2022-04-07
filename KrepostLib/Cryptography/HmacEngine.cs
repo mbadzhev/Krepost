67fc43654e85b7bc07c5177f992ceeca76d32cf8
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-04-07 21:01:30 +0100

    Add func to compute hmac

--- KrepostLib/Cryptography/HmacEngine.cs
+++ KrepostLib/Cryptography/HmacEngine.cs
@@ -1,7 +1,19 @@
-﻿namespace KrepostLib.Cryptography
+﻿using System.Security.Cryptography;
+
+namespace KrepostLib.Cryptography
 {
     public sealed class HmacEngine
     {
+        public static byte[] ComputeHmac(byte[] data, byte[] key, byte[] salt)
+        {
+            byte[] authenticationKey = Argon2Engine.DeriveKey(key, salt);
 
+            using (HMACSHA256 hmac = new HMACSHA256(authenticationKey))
+            {
+                Array.Clear(key, 0, key.Length);
+                Array.Clear(authenticationKey, 0, authenticationKey.Length);
+                return hmac.ComputeHash(data);
+            }
+        }
     }
 }
