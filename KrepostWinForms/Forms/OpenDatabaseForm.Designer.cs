61da6ddf01705d8eb6976e7209b480a65c817eb3
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-04-18 11:36:51 +0100

    Modify OpenDatabaseForm presentation to be more appealing

--- KrepostWinForms/Forms/OpenDatabaseForm.Designer.cs
+++ KrepostWinForms/Forms/OpenDatabaseForm.Designer.cs
@@ -31,26 +31,28 @@ namespace KrepostWinForms.Forms
         private void InitializeComponent()
         {
             System.Security.SecureString secureString1 = new System.Security.SecureString();
-            this.labelHeader = new System.Windows.Forms.Label();
+            this.labelTop = new System.Windows.Forms.Label();
             this.labelPassword = new System.Windows.Forms.Label();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonOpen = new System.Windows.Forms.Button();
             this.secureStringTextBox = new KrepostWinForms.UI.SecureStringTextBox();
+            this.labelDivider = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // labelHeader
+            // labelTop
             // 
-            this.labelHeader.AutoSize = true;
-            this.labelHeader.Location = new System.Drawing.Point(12, 9);
-            this.labelHeader.Name = "labelHeader";
-            this.labelHeader.Size = new System.Drawing.Size(153, 15);
-            this.labelHeader.TabIndex = 0;
-            this.labelHeader.Text = "Enter Your Master Password";
+            this.labelTop.AutoSize = true;
+            this.labelTop.Location = new System.Drawing.Point(12, 12);
+            this.labelTop.Margin = new System.Windows.Forms.Padding(3, 3, 3, 24);
+            this.labelTop.Name = "labelTop";
+            this.labelTop.Size = new System.Drawing.Size(126, 15);
+            this.labelTop.TabIndex = 0;
+            this.labelTop.Text = "Enter Master Password";
             // 
             // labelPassword
             // 
             this.labelPassword.AutoSize = true;
-            this.labelPassword.Location = new System.Drawing.Point(12, 86);
+            this.labelPassword.Location = new System.Drawing.Point(12, 58);
             this.labelPassword.Name = "labelPassword";
             this.labelPassword.Size = new System.Drawing.Size(99, 15);
             this.labelPassword.TabIndex = 2;
@@ -58,7 +60,7 @@ namespace KrepostWinForms.Forms
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(347, 326);
+            this.buttonCancel.Location = new System.Drawing.Point(397, 118);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 3;
@@ -68,7 +70,7 @@ namespace KrepostWinForms.Forms
             // 
             // buttonOpen
             // 
-            this.buttonOpen.Location = new System.Drawing.Point(266, 326);
+            this.buttonOpen.Location = new System.Drawing.Point(316, 118);
             this.buttonOpen.Name = "buttonOpen";
             this.buttonOpen.Size = new System.Drawing.Size(75, 23);
             this.buttonOpen.TabIndex = 4;
@@ -81,28 +83,38 @@ namespace KrepostWinForms.Forms
             this.secureStringTextBox.Data = secureString1;
             this.secureStringTextBox.DataHash = null;
             this.secureStringTextBox.DataSalt = null;
-            this.secureStringTextBox.Location = new System.Drawing.Point(117, 82);
+            this.secureStringTextBox.Location = new System.Drawing.Point(117, 54);
             this.secureStringTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.secureStringTextBox.Name = "secureStringTextBox";
-            this.secureStringTextBox.Size = new System.Drawing.Size(305, 23);
+            this.secureStringTextBox.Size = new System.Drawing.Size(354, 23);
             this.secureStringTextBox.TabIndex = 5;
+            // 
+            // labelDivider
+            // 
+            this.labelDivider.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelDivider.Location = new System.Drawing.Point(-13, 106);
+            this.labelDivider.Margin = new System.Windows.Forms.Padding(3, 24, 3, 7);
+            this.labelDivider.Name = "labelDivider";
+            this.labelDivider.Size = new System.Drawing.Size(510, 2);
+            this.labelDivider.TabIndex = 10;
             // 
             // OpenDatabaseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 361);
+            this.ClientSize = new System.Drawing.Size(484, 153);
+            this.Controls.Add(this.labelDivider);
             this.Controls.Add(this.secureStringTextBox);
             this.Controls.Add(this.buttonOpen);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.labelPassword);
-            this.Controls.Add(this.labelHeader);
+            this.Controls.Add(this.labelTop);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "OpenDatabaseForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "OpenDatabaseForm";
+            this.Text = "Open Database";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,10 +122,11 @@ namespace KrepostWinForms.Forms
 
         #endregion
 
-        private Label labelHeader;
+        private Label labelTop;
         private Label labelPassword;
         private Button buttonCancel;
         private Button buttonOpen;
         private SecureStringTextBox secureStringTextBox;
+        private Label labelDivider;
     }
 }
