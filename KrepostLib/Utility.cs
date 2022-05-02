430d2a95792aae51d4218c73c938100f036ce5e6
Momchil Badzhev <43140410+mbadzhev@users.noreply.github.com>
2022-05-02 23:48:41 +0100

    Modify documentation of classes in Lib namespace

--- KrepostLib/Utility.cs
+++ KrepostLib/Utility.cs
@@ -4,6 +4,12 @@ namespace KrepostLib
 {
     public static class Utility
     {
+        /// <summary>
+        /// Compares two instances of <see cref="string"/> datatype.
+        /// </summary>
+        /// <param name="str1">The first string to be compared.</param>
+        /// <param name="str2">The second string to be compared.</param>
+        /// <returns>True if the instances match, false if they do not match.</returns>
         public static bool CompareStrings(string str1, string str2)
         {
             if (str1 == null || str2 == null)
@@ -14,18 +20,32 @@ namespace KrepostLib
                 return false;
             return true;
         }
+        /// <summary>
+        /// Creates a stamp of the current time and date.
+        /// </summary>
+        /// <returns>The date and time stamp.</returns>
         public static string GetTimestamp()
         {
             DateTime time = DateTime.Now;
             return time.ToString("yyyyMMddHHmmssffff");
         }
+        /// <summary>
+        /// Converts a <see cref="string"/> to a byte array.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static byte[] ToByteArray(string value)
         {
             byte[] bytes = Encoding.UTF8.GetBytes(value);
             return bytes;
         }
+        /// <summary>
+        /// Displays the contents of a byte array in hex to the console.
+        /// </summary>
+        /// <param name="bytes"></param>
         public static void DisplayBytes(byte[] bytes)
         {
+            // Debugging only.
             for (int i = 0; i < bytes.Length; ++i)
             {
                 Console.Write(bytes[i].ToString("x2") + " ");
